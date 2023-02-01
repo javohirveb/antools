4800ded034432213dfdcc7648876f5710e7bc2cd
javohirveb <javohirtoxtasinov0001@gmail.com>
2023-02-01 13:49:12 +0500

    add testimonials section

--- javascript/app.js
+++ javascript/app.js
@@ -25,3 +25,32 @@ const likeRed = document.getElementById('like');
 likeRed.addEventListener('click', () => {
     like.classList.toggle('red');
 })
+
+// carousel
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("testimonials__slide");
+    let dots = document.getElementsByClassName("testimonials__dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";
+}
