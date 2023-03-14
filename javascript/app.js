a77259690903e34aab6854170237a8c0d16d3607
javohirveb <javohirtoxtasinov0001@gmail.com>
2023-03-14 10:58:34 +0500

    add likes foreach

--- javascript/app.js
+++ javascript/app.js
@@ -20,56 +20,14 @@ menuBtn.addEventListener('click', () => {
 })
 
 // like 
-let like1 = document.getElementById('like1');
-let like2 = document.getElementById('like2');
-let like3 = document.getElementById('like3');
-let like4 = document.getElementById('like4');
-let like5 = document.getElementById('like5');
-let like6 = document.getElementById('like6');
-let like7 = document.getElementById('like7');
-let like8 = document.getElementById('like8');
-let like9 = document.getElementById('like9');
-let like10 = document.getElementById('like10');
-
-like1.addEventListener('click', () => {
-    like1.classList.toggle('red');
-})
-
-like2.addEventListener('click', () => {
-    like2.classList.toggle('red');
-})
-
-like3.addEventListener('click', () => {
-    like3.classList.toggle('red');
-})
-
-like4.addEventListener('click', () => {
-    like4.classList.toggle('red');
-})
-
-like5.addEventListener('click', () => {
-    like5.classList.toggle('red');
-})
-
-like6.addEventListener('click', () => {
-    like6.classList.toggle('red');
-})
-
-like7.addEventListener('click', () => {
-    like7.classList.toggle('red');
-})
-
-like8.addEventListener('click', () => {
-    like8.classList.toggle('red');
-})
-
-like9.addEventListener('click', () => {
-    like9.classList.toggle('red');
-})
-
-like10.addEventListener('click', () => {
-    like10.classList.toggle('red');
-})
+const likes = document.querySelectorAll(".fa-heart")
+console.log(likes);
+likes.forEach(element => {
+   element.addEventListener("click", () => {
+      console.log("red");
+      element.classList.toggle("red")
+   })
+});
 
 // carousel
 let slideIndex = 1;
